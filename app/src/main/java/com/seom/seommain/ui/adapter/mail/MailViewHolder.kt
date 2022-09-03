a1239c2f4bacfa9ae4aa3e6b9746ec0e81030109
aurora32s <aurora32s@naver.com>
2022-09-03 19:16:33 +0900

    [REFACTOR] #11 - databinding 추가 및 layout 구조 변경

--- app/src/main/java/com/seom/seommain/ui/adapter/mail/MailViewHolder.kt
+++ app/src/main/java/com/seom/seommain/ui/adapter/mail/MailViewHolder.kt
@@ -9,26 +9,23 @@ import com.seom.seommain.ui.model.mail.MailModel
 import com.seom.seommain.ui.model.mail.ProfileImageType
 
 class MailViewHolder(private val binding: ItemMailBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(mail: MailModel) = with(binding) {
-        senderTextView.text = mail.sender
-        titleTextView.text = mail.title
-        contentTextView.text = mail.content
-        dateTextView.text = mail.date
-
-        // profile image 설정
-        if (mail.profileImageType === ProfileImageType.LETTER) {
-            // sender 이름이 영어로 시작하는 경우
-            profileimageView.isGone = true
-            profileTextView.isVisible = true
-
-            profileTextView.text = mail.sender.first().toString()
-            senderProfileImage.backgroundTintList = ContextCompat.getColorStateList(
-                binding.root.context,
-                mail.profileBackgroundColor
-            );
-        } else {
-            profileimageView.isVisible = true
-            profileTextView.isGone = true
-        }
+    fun bind(mail: MailModel) {
+        binding.mail = mail
+        
+//        // profile image 설정
+//        if (mail.profileImageType === ProfileImageType.LETTER) {
+//            // sender 이름이 영어로 시작하는 경우
+//            profileimageView.isGone = true
+//            profileTextView.isVisible = true
+//
+//            profileTextView.text = mail.sender.first().toString()
+//            senderProfileImage.backgroundTintList = ContextCompat.getColorStateList(
+//                binding.root.context,
+//                mail.profileBackgroundColor
+//            );
+//        } else {
+//            profileimageView.isVisible = true
+//            profileTextView.isGone = true
+//        }
     }
 }
